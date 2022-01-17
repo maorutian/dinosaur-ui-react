72af39d2adc7b1b34d7a114b115b788e79762b47
maoru <maorutian@gmail.com>
2022-01-16 21:11:28 -0500

    Menu component: highlight selected menu item

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import classNames from 'classnames';
 
 type MenuMode = 'horizontal' | 'vertical';
+type SelectCallback = (selectedIndex: number) => void;
 
 interface MenuProps {
     className?: string;
     defaultIndex?: number;
     mode?: MenuMode;
     style?: React.CSSProperties;
+    onSelect?: SelectCallback;
 }
 
+interface IMenuContext {
+    index: number;
+    onSelect?: SelectCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({index: 0});
+
 const Menu: React.FC<MenuProps> = (props) => {
-    const {children, className, defaultIndex, mode, style} = props;
+    const {children, className, defaultIndex, mode, style, onSelect} = props;
     const classes = classNames('dinosaur-menu', className, {
         'menu-vertical': mode === 'vertical'
     });
 
+    //highlight active/selected menu item
+    const [currentActive, setActive] = useState(defaultIndex);
+    const handleClick = (index: number) => {
+        setActive(index);
+        if (onSelect) {
+            onSelect(index);
+        }
+    }
+    const passedContext: IMenuContext = {
+        index: currentActive ? currentActive : 0,
+        onSelect: handleClick,
+    }
+
     return (
         <ul className={classes} style={style}>
-            {children}</ul>
+            <MenuContext.Provider value={passedContext}>
+                {children}
+            </MenuContext.Provider>
+        </ul>
     );
 }
 
--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classNames from 'classnames';
+import {MenuContext} from './menu';
 
-interface MenuItemProps {
+export interface MenuItemProps {
     className?: string,
-    index?: number,
+    index: number,
     disabled?: boolean,
     style?: React.CSSProperties;
 }
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
     const {children, className, index, disabled, style} = props;
+    const context = useContext(MenuContext);
     const classes = classNames('menu-item', className, {
         'is-disabled': disabled,
+        'is-active': context.index === index
     });
+    const handleClick = () => {
+        if (context.onSelect && !disabled) {
+            context.onSelect(index);
+        }
+    };
     return (
-        <li className={classes} style={style}>
+        <li className={classes} style={style} onClick={handleClick}>
             {children}
         </li>
     );
