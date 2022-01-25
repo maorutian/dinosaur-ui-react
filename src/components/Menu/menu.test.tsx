9d318e3f9c18fd7f01e46c52d54ffeda267d2404
maoru <maorutian@gmail.com>
2022-01-24 22:49:02 -0500

    test: test Menu components (menu,subMenu,menuItem) in horizontal and vertical modes

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import {render, RenderResult, fireEvent, cleanup} from '@testing-library/react';
+import {render, RenderResult, fireEvent, wait} from '@testing-library/react';
 import Menu, {MenuProps} from './menu';
 import MenuItem from './menuItem';
+import SubMenu from './subMenu';
 
-//test default
+//test default (horizontal mode)
 const testProps: MenuProps = {
-    defaultIndex: 0,
+    defaultIndex: "0",
     onSelect: jest.fn(),
     className: 'custom'
 };
 
-//test mode
+//test horizontal mode
 const testVerProps: MenuProps = {
-    defaultIndex: 0,
-    mode: 'vertical'
+    defaultIndex: "0",
+    mode: 'vertical',
+    defaultOpenSubMenus: ['4']
 };
 
 const generateMenu = (props: MenuProps) => {
@@ -28,47 +30,121 @@ const generateMenu = (props: MenuProps) => {
             <MenuItem>
                 item
             </MenuItem>
+            <SubMenu title="dropdown">
+                <MenuItem>
+                    drop1
+                </MenuItem>
+            </SubMenu>
+            <SubMenu title="opened">
+                <MenuItem>
+                    opened1
+                </MenuItem>
+            </SubMenu>
         </Menu>
     )
 };
+
+//create css style for open menu
+const createStyleFile = () => {
+    const cssFile: string = `
+    .dinosaur-submenu {
+      display: none;
+    }
+    .dinosaur-submenu.menu-opened {
+      display:block;
+    }
+  `;
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = cssFile;
+    return style;
+}
 
 let wrapper: RenderResult,
     menuElement: HTMLElement,
     activeElement: HTMLElement,
     disabledElement: HTMLElement;
 
-describe('test Menu and MenuItem component', () => {
+describe('test Menu, SubMenu and MenuItem component (horizontal model)', () => {
     beforeEach(() => {
         wrapper = render(generateMenu(testProps));
+        //add css
+        wrapper.container.append(createStyleFile());
         menuElement = wrapper.getByTestId('test-menu');
         activeElement = wrapper.getByText('active');
         disabledElement = wrapper.getByText('disabled');
     })
 
-    it('should render correct Menu and MenuItem based on default props', () => {
+    it('should render correct Menu, subMenu and MenuItem based on default props', () => {
         expect(menuElement).toBeInTheDocument();
         expect(menuElement).toHaveClass('dinosaur-menu custom');
-        expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+        //menu item's li (3) + submenu's li (2) + menu items in the submenu's li (2)
+        expect(menuElement.getElementsByTagName('li').length).toEqual(7);
+        //menu item's li (3) + submenu's li (2)
+        expect(menuElement.querySelectorAll(':scope > li').length).toEqual(5)
         expect(activeElement).toHaveClass('menu-item is-active');
         expect(disabledElement).toHaveClass('menu-item is-disabled');
     })
 
     it('click items should change to is-active and call the right callback', () => {
+        //click normal item, it should change to active
         const thirdItem = wrapper.getByText('item');
         fireEvent.click(thirdItem);
         expect(thirdItem).toHaveClass('is-active');
+        //old active item is not active after clicked another one
         expect(activeElement).not.toHaveClass('is-active');
-        expect(testProps.onSelect).toHaveBeenCalledWith(2);
+        expect(testProps.onSelect).toHaveBeenCalledWith('2');
+        //disabled item wouldn't be active
         fireEvent.click(disabledElement);
         expect(disabledElement).not.toHaveClass('is-active');
-        expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+        expect(testProps.onSelect).not.toHaveBeenCalledWith('1');
+    })
+
+    it('should show dropdown items when hover on subMenu and call the right callback', async () => {
+        const dropdownElement = wrapper.getByText('dropdown');
+        const menuItemInSubmenu = wrapper.getByText('drop1');
+        //subMenu is close
+        expect(menuItemInSubmenu).not.toBeVisible();
+        //show dropdown items when hover on subMenu
+        fireEvent.mouseEnter(dropdownElement);
+        await wait(() => {
+            expect(menuItemInSubmenu).toBeVisible();
+        })
+        //menuitem in submenu is active and call the right callback
+        fireEvent.click(menuItemInSubmenu);
+        expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+        expect(menuItemInSubmenu).toHaveClass('is-active');
+        //hide dropdown when mouse leave
+        fireEvent.mouseLeave(dropdownElement);
+        await wait(() => {
+            expect(wrapper.getByText('drop1')).not.toBeVisible();
+        })
+    })
+})
+
+describe('test Menu, SubMenu and MenuItem component (vertical model)', () => {
+    beforeEach(() => {
+        wrapper = render(generateMenu(testVerProps));
+        //add css
+        wrapper.container.append(createStyleFile());
     })
 
     it('should render vertical mode when mode is set to vertical', () => {
-        //remove wrapper = render(generateMenu(testProps))
-        cleanup();
-        const wrapper = render(generateMenu(testVerProps));
         const menuElement = wrapper.getByTestId('test-menu');
         expect(menuElement).toHaveClass('menu-vertical');
+    })
+
+    it('should show dropdown items when click on subMenu and call the right callback for vertical mode', () => {
+        const dropdownElement = wrapper.getByText('dropdown');
+        const menuItemInSubmenu = wrapper.getByText('drop1');
+        //subMenu is close
+        expect(menuItemInSubmenu).not.toBeVisible();
+        //show dropdown items when click on subMenu
+        fireEvent.click(dropdownElement);
+        expect(menuItemInSubmenu).toBeVisible();
+        //menuitem in submenu is active and call the right callback
+        fireEvent.click(menuItemInSubmenu);
+        expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+        expect(menuItemInSubmenu).toHaveClass('is-active');
     })
 })
