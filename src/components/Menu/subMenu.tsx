21aa7dda823f106feda852ba258c6c5a56b015cb
maoru <maorutian@gmail.com>
2022-02-01 22:40:41 -0500

    feat: add animation for subMenu

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,5 +1,6 @@
 import React, {useContext, FunctionComponentElement, useState} from 'react';
 import classNames from 'classnames';
+import {CSSTransition} from 'react-transition-group';
 import {MenuContext} from './menu';
 import {MenuItemProps} from './menuItem';
 import Icon from '../Icon/icon';
@@ -65,9 +66,16 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
             }
         });
         return (
-            <ul className={subMenuClasses}>
-                {childrenComponent}
-            </ul>
+            <CSSTransition
+                in={menuOpen}
+                timeout={300}
+                classNames="zoom-in-top"
+                appear
+            >
+                <ul className={subMenuClasses}>
+                    {childrenComponent}
+                </ul>
+            </CSSTransition>
         )
     };
 
